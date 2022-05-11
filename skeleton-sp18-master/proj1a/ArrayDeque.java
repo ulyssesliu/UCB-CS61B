468782954d3a5f56371524069a43ca8680baf180
Chuang Liu <ulysses_liu@sjtu.edu.cn>
2022-05-11 15:02:42 +0800

    add proj1a

--- skeleton-sp18-master/proj1a/ArrayDeque.java
+++ skeleton-sp18-master/proj1a/ArrayDeque.java
@@ -1,2 +1,36 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private class Items{
+        T[] itemArr;
+        int num; // number of elements stored in the array.
+        int volume; // number of memory blocks preserved by array.
+
+        // constructors
+        public Items(){
+            itemArr = (T []) new Object[100];
+            this.volume = 100;
+            this.num = 0;
+        }
+        public Items(int volume){
+            itemArr = (T []) new Object[volume];
+            this.volume = volume;
+            this.num = 0;
+        }
+    }
+
+    private Items items;
+
+    // constructors
+    public ArrayDeque(){
+        this.items = new Items();
+    }
+    public ArrayDeque(int volume){
+        this.items = new Items(volume);
+    }
+
+    // methods
+    public void addFirst(T item){
+
+    }
+
+
 }
