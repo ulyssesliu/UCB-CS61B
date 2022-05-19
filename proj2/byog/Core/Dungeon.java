ab767a86b214dc8b7ff9d21eb2faa02b44f4d855
chuang <ulysses_liu@sjtu.edu.cn>
2022-05-19 17:50:25 +0800

    update addRoom logic in Dungeon.java

--- proj2/byog/Core/Dungeon.java
+++ proj2/byog/Core/Dungeon.java
@@ -26,18 +26,18 @@ public class Dungeon {
             }
         }
     }
-
-    public void addRoom(Room newRoom){// TODO(done): Add a new room to the dungeonMap (after it passes the test.)
-        if(isConflict(newRoom)){
+    
+    public void addRoom(Room newRoom, Random random){// TODO(done): Add a new room to the dungeonMap (after it passes the test.)
+        int[] offSet = getRandomOffset(random);
+        if(isConflict(newRoom, offSet)){
             return;
         }
-        addRoomToMap(newRoom);
+        addRoomToMap(newRoom, offSet);
         addRoomToList(newRoom);
     }
 
     // TODO: connect the discrete distributed Rooms in dungeonMap.
     private void connect(){
-
     }
 
     // TODO: sort the rooms in the list by their xOff
@@ -45,41 +45,39 @@ public class Dungeon {
 
     }
 
-
-    private boolean isConflict(Room newRoom){// TODO (done): Test whether the Room can fit into the given map (2d Tile array)
-        return !((this.width > newRoom.width + newRoom.xDoorPos) && (this.height > newRoom.height + newRoom.yDoorPos));
+    
+    private boolean isConflict(Room newRoom, int[] offSet){// TODO (done): Test whether the Room can fit into the given map (2d Tile array)
+        return !((this.width > newRoom.width + offSet[0]) && (this.height > newRoom.height + offSet[1]));
     }
-    private void addRoomToMap(Room newRoom){
+    private void addRoomToMap(Room newRoom, int[] offSet){
         // fill the floors
         for(int xOff = 1; xOff < newRoom.width - 1; xOff++){
             for(int yOff = 1; yOff < newRoom.height - 1; yOff++){
-                this.dungeonMap[xOff + newRoom.xDoorPos][yOff + newRoom.yDoorPos] = Tileset.FLOOR;
+                this.dungeonMap[xOff + offSet[0]][yOff + offSet[1]] = Tileset.FLOOR;
             }
         }
 
         // fill the walls
         // the upper and lower walls
         for(int xOff = 0; xOff < newRoom.width; xOff++){
-            this.dungeonMap[xOff + newRoom.xDoorPos][newRoom.yDoorPos] = Tileset.WALL; // lower wall
-            this.dungeonMap[xOff + newRoom.xDoorPos][newRoom.yDoorPos + newRoom.height - 1] = Tileset.WALL; // upper wall
+            this.dungeonMap[xOff + offSet[0]][offSet[1]] = Tileset.WALL; // lower wall
+            this.dungeonMap[xOff + offSet[0]][offSet[1] + newRoom.height - 1] = Tileset.WALL; // upper wall
         }
         // the left and right walls
         for(int yOff = 1; yOff < newRoom.height - 1; yOff++){
-            this.dungeonMap[newRoom.xDoorPos][yOff + newRoom.yDoorPos] = Tileset.WALL; // left wall
-            this.dungeonMap[newRoom.xDoorPos + newRoom.width - 1][yOff + newRoom.yDoorPos] = Tileset.WALL; // right wall
+            this.dungeonMap[offSet[0]][yOff + offSet[1]] = Tileset.WALL; // left wall
+            this.dungeonMap[offSet[0] + newRoom.width - 1][yOff + offSet[1]] = Tileset.WALL; // right wall
         }
     }
     private void addRoomToList(Room newRoom){
         this.roomList.add(newRoom);
     }
-
+    
     private int[] getRandomOffset(Random random){ // get random offsets xOff and yOff
         int[] offSet = new int[2];
         offSet[0] = RandomUtils.uniform(random, this.width-3);
         offSet[1] = RandomUtils.uniform(random, this.height-3);
         return offSet;
     }
-
-
 
 }
